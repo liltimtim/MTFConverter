dac1eeda1e84d331ceb0a5de8e9bdcfdcc1e44d0
Timothy Barrett <liltimtim@gmail.com>
2016-09-18 14:33:57 -0400

    initial repo creation

---
+++ Example/Pods/Target Support Files/MTFConverter/MTFConverter-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MTFConverter : NSObject
+@end
+@implementation PodsDummy_MTFConverter
+@end
---
+++ Example/Pods/Target Support Files/Pods-MTFConverter_Example/Pods-MTFConverter_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_MTFConverter_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_MTFConverter_ExampleVersionString[];
+
