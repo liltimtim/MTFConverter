0fef6521b59cfde0a21ca53109969550855b3b19
Timothy Barrett <liltimtim@gmail.com>
2016-09-18 20:19:02 -0400

    added first functions and unit test.  Feeling out how this is going to work.

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -4,6 +4,13 @@ import MTFConverter
 
 class MTFconverterTests : XCTestCase {
     func testConvertJSONToForm() {
-        
+        var simpleForm = [String:AnyObject]()
+        simpleForm["key1"] = "hello world"
+        simpleForm["key2"] = 100.0
+        guard let gForm = MTFConverter.convertJSONForm(simpleForm) else {
+            XCTFail()
+            return
+        }
+        XCTAssertEqual(gForm.allRows.count, 2)
     }
 }
--- MTFConverter/Classes/MTFConverter.swift
+++ MTFConverter/Classes/MTFConverter.swift
@@ -10,7 +10,40 @@ import UIKit
 import Eureka
 public class MTFConverter: NSObject {
     
-    public static func convertJSONForm(json:AnyObject) {
-        
+    public static func convertJSONForm(json:[String:AnyObject]) -> Form? {
+        var form = Form()
+        var rows = [BaseRow]()
+        for (key, value) in json {
+            print(key)
+            print(value)
+            guard let aRow = MTFConverter.returnRowType(withKeyedTag: key, withValue: value) else {
+                print("invalid row")
+                return nil
+            }
+            rows.append(aRow)
+        }
+        form += [Section()]
+        form.allSections.first!.appendContentsOf(rows)
+        return form
+    }
+    
+    public static func returnRowType(withKeyedTag tag:String, withValue value:AnyObject) -> BaseRow? {
+        switch (value) {
+        case is String:
+            print("I'm a string")
+            let textRow = TextRow(tag) {
+                $0.title = "Hello world"
+            }
+            return textRow
+        case is Double:
+            print("I'm a double")
+            return DecimalRow(tag: tag)
+            
+        default:
+            print("error")
+            assertionFailure("Bad data type given to converter")
+            return nil
+        }
+        return nil
     }
 }
